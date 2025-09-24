8b29f88de62e611954c6c7781d372360d02252d9
ClaudiaSheinbaum <gatogordogigante@gmail.com>
2025-09-24 16:57:53 -0700

    git init

---
+++ src/main/java/com/mycompany/jpatienda/JPATienda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jpatienda;
+
+/**
+ *
+ * @author LABCISCO-PC107
+ */
+public class JPATienda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
